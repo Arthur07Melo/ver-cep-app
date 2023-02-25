908dbc2af4986b778a43ab7518e2e9a5baf6a6da
Arthur07Melo <aarthur00melo@gmail.com>
2023-02-24 21:54:36 -0300

    Adicionado error handler e loading enquanto não resulta.

--- src/components/cepBox.tsx
+++ src/components/cepBox.tsx
@@ -5,16 +5,23 @@ import useFetch from "@/hooks/useFetch";
 export default function CepBox(props: {CEP: number}){
     let { data: CEPinfos, error, isFetching } = useFetch(`https://viacep.com.br/ws/${props.CEP}/json/`, props.CEP);
 
+
     return(
-        <div className="flex flex-col items-center justify-center w-full">
-        {Object.keys(CEPinfos).map((element, i) => {
-          return (
-            <div className="flex items-center" key={i}>
-              <h1 className="text-green-700 text-lg font-extrabold">{element}: </h1>
-              <h1 className="font-bold">{CEPinfos[(element as keyof typeof CEPinfos)]}</h1>
-            </div>
-          )
-        })}
+        <div className="flex items-center justify-center w-full">
+        <div className="flex flex-col">
+          {error?.message=="Network Error" && <h1 className="text-red-700 text-lg font-extrabold">Error: CEP inválido</h1>}
+
+          {isFetching && <h1 className="text-green-700 text-lg font-extrabold">Carregando...</h1>}
+
+          {Object.keys(CEPinfos).map((element, i) => {
+            return (
+              <div className="flex items-center" key={i}>
+                <h1 className="text-green-700 text-lg font-extrabold">{element}: </h1>
+                <h1 className="font-bold">{CEPinfos[(element as keyof typeof CEPinfos)]}</h1>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
 }
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -5,14 +5,16 @@ import { useState, useEffect } from "react";
 
 export default function useFetch(url: string, stage:number) {
     const [data, setData] = useState({});
-    const [isFetching, setIsFetching] = useState(true);
+    const [isFetching, setIsFetching] = useState(false);
     const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
         if(stage!==0){
+            setIsFetching(true);
             axios.get(url)
                 .then(response => {
                     setData(response.data);
+                    setError(null);
                     console.log("data setted");
                 })
                 .catch(err => {
